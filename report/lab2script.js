d7900b8b816c12a6040d81eb7940ceb620f7d301
Alexey <lesha3320@gmail.com>
2023-06-01 17:29:08 +0300

    + add lab2 report

--- report/lab2script.js
+++ report/lab2script.js
@@ -1,4 +1,4 @@
-const lab2Info = {
+export const lab2Info = {
   topic:
     "Тема: КАСКАДНІ ТАБЛИЦІ СТИЛІВ. СЕЛЕКТОРИ .ІДЕНТИФІКАТОРИ. СТИЛЬОВЕ ОФОРМЛЕННЯ ТЕКСТОВИХ ЕЛЕМЕНТІВ В HTML-ДОКУМЕНТАХ.",
   goal:
@@ -15,3 +15,198 @@ const lab2Info = {
   conclusion:
     "Висновки роботи: виконуючи лабораторну роботу 2 ми навчилися працювати з селекторами, списками, ідентифікаторами, відступами, плаваючими елементами, та покращили навички оформлення текстових елементів",
 };
+
+export function setupSecondLabView() {
+  let labNavView = document.getElementById("lab-nav-view");
+
+  // очищуємо div від дочірніх елементів, щоб не було дублювання
+  while (labNavView.firstChild) {
+    labNavView.removeChild(labNavView.firstChild);
+  }
+
+  let topicDiv = document.createElement("div");
+  topicDiv.classList.add("chapter");
+  topicDiv.innerHTML = "Тема, мета, ЛР №2 Місце розташування сайту, звіту";
+  topicDiv.addEventListener("click", showTopicAndGoalInfo);
+
+  let styleMethodsDiv = document.createElement("div");
+  styleMethodsDiv.classList.add("chapter");
+  styleMethodsDiv.innerHTML = "Способи підключення стилів";
+  styleMethodsDiv.addEventListener("click", showStyleMethods);
+
+  let selectorsDiv = document.createElement("div");
+  selectorsDiv.classList.add("chapter");
+  selectorsDiv.innerHTML = "СЕЛЕКТОРИ";
+  //   selectorsDiv.addEventListener("click", showTagSelectors);
+
+  let tagSelectorsDiv = document.createElement("div");
+  tagSelectorsDiv.classList.add("chapter");
+  tagSelectorsDiv.innerHTML = "Селектори тегу";
+  tagSelectorsDiv.addEventListener("click", showTagSelectors);
+
+  let classSelectorsDiv = document.createElement("div");
+  classSelectorsDiv.classList.add("chapter");
+  classSelectorsDiv.innerHTML = "Селектори класу";
+  classSelectorsDiv.addEventListener("click", showClassSelectors);
+
+  let idSelectorsDiv = document.createElement("div");
+  idSelectorsDiv.classList.add("chapter");
+  idSelectorsDiv.innerHTML = "Селектори ідентифікаторів";
+  idSelectorsDiv.addEventListener("click", showIdSelectors);
+
+  let otherSelectorsDiv = document.createElement("div");
+  otherSelectorsDiv.classList.add("chapter");
+  otherSelectorsDiv.innerHTML = "Інші селектори";
+  otherSelectorsDiv.addEventListener("click", showOtherSelectors);
+
+  let cssInfoDiv = document.createElement("div");
+  cssInfoDiv.classList.add("chapter");
+  cssInfoDiv.innerHTML =
+    "CSS: Шрифти Текст Таблиці Фон Контур Списки CSS Просунутий";
+  cssInfoDiv.addEventListener("click", showCssInfo);
+
+  let conclusionDiv = document.createElement("div");
+  conclusionDiv.classList.add("chapter");
+  conclusionDiv.innerHTML = "ВИСНОВКИ до ЛР №2";
+  conclusionDiv.addEventListener("click", showConclusion);
+
+  labNavView.appendChild(topicDiv);
+  labNavView.appendChild(styleMethodsDiv);
+  labNavView.appendChild(selectorsDiv);
+  labNavView.appendChild(tagSelectorsDiv);
+  labNavView.appendChild(classSelectorsDiv);
+  labNavView.appendChild(idSelectorsDiv);
+  labNavView.appendChild(otherSelectorsDiv);
+  labNavView.appendChild(cssInfoDiv);
+  labNavView.appendChild(conclusionDiv);
+}
+
+export function showTopicAndGoalInfo() {
+  let placeOut = document.getElementById("place-out");
+  placeOut.textContent = "";
+
+  let topic = document.createElement("p");
+  topic.textContent = lab2Info.topic;
+
+  let goal = document.createElement("p");
+  goal.textContent = lab2Info.goal;
+
+  placeOut.appendChild(topic);
+  placeOut.appendChild(goal);
+}
+
+export function showStyleMethods() {
+  let placeOut = document.getElementById("place-out");
+  placeOut.textContent = "";
+
+  let p = document.createElement("p");
+  p.textContent = lab2Info.style_methods;
+
+  placeOut.appendChild(p);
+}
+
+export function showTagSelectors() {
+  let placeOut = document.getElementById("place-out");
+  placeOut.textContent = "";
+
+  let contentDiv = document.createElement("div");
+  contentDiv.style.paddingTop = "20px";
+  contentDiv.style.paddingBottom = "10px";
+  contentDiv.style.maxHeight = "580px";
+  contentDiv.style.overflowX = "auto";
+  contentDiv.style.overflowY = "auto";
+
+  let img = document.createElement("img");
+  img.src = "assets/lab2/tag_selector.png";
+  img.style.width = "1000px";
+  img.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv).appendChild(img);
+}
+
+export function showClassSelectors() {
+  let placeOut = document.getElementById("place-out");
+  placeOut.textContent = "";
+
+  let contentDiv = document.createElement("div");
+  contentDiv.style.paddingTop = "20px";
+  contentDiv.style.paddingBottom = "10px";
+  contentDiv.style.maxHeight = "580px";
+  contentDiv.style.overflowX = "auto";
+  contentDiv.style.overflowY = "auto";
+
+  let img = document.createElement("img");
+  img.src = "assets/lab2/class_selector.png";
+  img.style.width = "1000px";
+  img.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv).appendChild(img);
+}
+
+export function showIdSelectors() {
+  let placeOut = document.getElementById("place-out");
+  placeOut.textContent = "";
+
+  let contentDiv = document.createElement("div");
+  contentDiv.style.paddingTop = "20px";
+  contentDiv.style.paddingBottom = "10px";
+  contentDiv.style.maxHeight = "580px";
+  contentDiv.style.overflowX = "auto";
+  contentDiv.style.overflowY = "auto";
+
+  let img = document.createElement("img");
+  img.src = "assets/lab2/id_selector.png";
+  img.style.width = "1000px";
+  img.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv).appendChild(img);
+}
+
+export function showOtherSelectors() {
+  let placeOut = document.getElementById("place-out");
+  placeOut.textContent = "";
+
+  let contentDiv = document.createElement("div");
+  contentDiv.style.paddingTop = "20px";
+  contentDiv.style.paddingBottom = "10px";
+  contentDiv.style.maxHeight = "580px";
+  contentDiv.style.overflowX = "auto";
+  contentDiv.style.overflowY = "auto";
+
+  let img = document.createElement("img");
+  img.src = "assets/lab2/other_selector.png";
+  img.style.width = "1000px";
+  img.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv).appendChild(img);
+}
+
+export function showCssInfo() {
+  let placeOut = document.getElementById("place-out");
+  placeOut.textContent = "";
+
+  let contentDiv = document.createElement("div");
+  contentDiv.style.paddingTop = "20px";
+  contentDiv.style.paddingBottom = "10px";
+  contentDiv.style.maxHeight = "975px";
+  contentDiv.style.overflowX = "auto";
+  contentDiv.style.overflowY = "auto";
+
+  let img = document.createElement("img");
+  img.src = "assets/lab2/css_info.png";
+  img.style.width = "1000px";
+  img.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv).appendChild(img);
+}
+
+export function showConclusion() {
+  let placeOut = document.getElementById("place-out");
+  placeOut.textContent = "";
+
+  let p = document.createElement("p");
+  p.textContent = lab2Info.conclusion;
+  p.style.fontSize = "20px";
+
+  placeOut.appendChild(p);
+}
--- report/script.js
+++ report/script.js
@@ -1,6 +1,5 @@
-// import { name } from "./lab2script.js";
-// import { name2 } from "./lab3script.js";
 import * as lab1 from "./lab1script.js";
+import * as lab2 from "./lab2script.js";
 
 // завантажуємо відображенння навішації для першої лаби
 document.addEventListener("DOMContentLoaded", lab1.setupFirstLabView);
@@ -8,5 +7,7 @@ document.addEventListener("DOMContentLoaded", lab1.setupFirstLabView);
 function updateInfo(modelNumber) {
   if (modelNumber === 1) {
     lab1.setupFirstLabView();
+  } else if (modelNumber === 2) {
+    lab2.setupSecondLabView();
   }
 }
