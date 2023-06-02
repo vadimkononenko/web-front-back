8a56f505f27cb04f18e99412202e733c31acf740
Vadim Kononenko <vadiskon@gmail.com>
2023-06-02 09:12:42 +0300

    * simplified first loading view

--- report/script.js
+++ report/script.js
@@ -1,18 +1,9 @@
 import * as lab1 from "./lab1script.js";
 import * as lab2 from "./lab2script.js";
 
-// завантажуємо відображенння навішації для першої лаби
-document.addEventListener("DOMContentLoaded", lab1.setupFirstLabView);
+lab1.setupFirstLabView();
 
 const laba1 = document.getElementById("laba1");
 laba1.onclick = lab1.setupFirstLabView;
 const laba2 = document.getElementById("laba2");
 laba2.onclick = lab2.setupSecondLabView;
-
-// function updateInfo(modelNumber) {
-//   if (modelNumber === 1) {
-//     lab1.setupFirstLabView();
-//   } else if (modelNumber === 2) {
-//     lab2.setupSecondLabView();
-//   }
-// }
