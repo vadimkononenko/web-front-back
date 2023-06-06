8c708327d36a4f3dacdbdab7a0dc9fb008cab095
Alexey <lesha3320@gmail.com>
2023-06-06 17:33:32 +0300

    * lab3 report

--- report/lab3script.js
+++ report/lab3script.js
@@ -7,15 +7,21 @@ export const lab3Info = {
   goal:
     "Мета: придбати практичні навички роботи верстки сторінок засобами CSS, верстки на основі плаваючих елементів, з’ясувати переваги та недоліки типів макетів веб-сторінок, " +
     "придбати практичні навички роботи верстки сторінок засобами CSS та FLEXBOX",
-  template_task: "./assets/lab3/template.png",
+  template_task: "./assets/lab3/task1.png",
+  template_code: "./assets/lab3/task1_html.png",
   tableTask: {
-    fixed_table: "./assets/lab3/fixedTable.png",
-    rubber_table: "./assets/lab3/rubberTable.png",
+    task2_23: "./assets/lab3/task2_23.png",
+    task2_23table: "./assets/lab3/task2_23table.png",
+    task23_div_fixed: "./assets/lab3/task23_div_fixed.png",
+    task23_div_rubber: "./assets/lab3/task23_div_rubber.png",
   },
   flexTask: {
-    page_screen: "",
-    page_html: "",
-    page_css: "",
+    flexArScreen: "./assets/lab3/flexArScreen.png",
+    flexVkScreen: "./assets/lab3/flexVkScreen.png",
+    flexArHtml: "./assets/lab3/flexArHtml.png",
+    flexVkHtml: "./assets/lab3/flexVkHtml.png",
+    flexArCss: "./assets/lab3/flexArCss.png",
+    flexVkCss: "./assets/lab3/flexVkCss.png",
   },
   conclusion:
     "Висновки роботи: виконуючи лабораторну роботу 3 ми придбали практичні навички верстки сторінок з CSS та з'ясували переваги та недоліки типів макетів веб-сторінок. Також ми придбали навички верстки сторінок з CSS та FLEXBOX",
@@ -49,12 +55,12 @@ export function setupSecondLabView() {
 
   let fixedTableDiv = document.createElement("div");
   fixedTableDiv.classList.add("inner-chapter");
-  fixedTableDiv.innerHTML = "Фіксована таблична верстка";
+  fixedTableDiv.innerHTML = "Таблична верстка";
   fixedTableDiv.addEventListener("click", showFixedTable);
 
   let rubberTableDiv = document.createElement("div");
   rubberTableDiv.classList.add("inner-chapter");
-  rubberTableDiv.innerHTML = "Фіксована болокова верстка";
+  rubberTableDiv.innerHTML = "Болокова верстка";
   rubberTableDiv.addEventListener("click", showRubberTable);
 
   let flexTaskDiv = document.createElement("div");
@@ -138,7 +144,7 @@ export function showTemplate() {
   let contentDiv = document.createElement("div");
   contentDiv.style.paddingTop = "20px";
   contentDiv.style.paddingBottom = "10px";
-  contentDiv.style.maxHeight = "580px";
+  contentDiv.style.maxHeight = "850px";
   contentDiv.style.overflowX = "auto";
   contentDiv.style.overflowY = "auto";
 
@@ -147,7 +153,14 @@ export function showTemplate() {
   img.style.width = "1000px";
   img.style.objectFit = "scale-down";
 
-  placeOut.appendChild(contentDiv).appendChild(img);
+  let img2 = document.createElement("img");
+  img2.src = lab3Info.template_code;
+  img2.style.width = "1000px";
+  img2.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv);
+  contentDiv.appendChild(img);
+  contentDiv.appendChild(img2);
 }
 
 export function showFixedTable() {
@@ -156,16 +169,23 @@ export function showFixedTable() {
   let contentDiv = document.createElement("div");
   contentDiv.style.paddingTop = "20px";
   contentDiv.style.paddingBottom = "10px";
-  contentDiv.style.maxHeight = "580px";
+  contentDiv.style.maxHeight = "850px";
   contentDiv.style.overflowX = "auto";
   contentDiv.style.overflowY = "auto";
 
   let img = document.createElement("img");
-  img.src = lab3Info.tableTask.fixed_table;
+  img.src = lab3Info.tableTask.task2_23;
   img.style.width = "1000px";
   img.style.objectFit = "scale-down";
 
-  placeOut.appendChild(contentDiv).appendChild(img);
+  let img2 = document.createElement("img");
+  img2.src = lab3Info.tableTask.task2_23table;
+  img2.style.width = "1000px";
+  img2.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv);
+  contentDiv.appendChild(img);
+  contentDiv.appendChild(img2);
 }
 
 export function showRubberTable() {
@@ -174,16 +194,29 @@ export function showRubberTable() {
   let contentDiv = document.createElement("div");
   contentDiv.style.paddingTop = "20px";
   contentDiv.style.paddingBottom = "10px";
-  contentDiv.style.maxHeight = "580px";
+  contentDiv.style.maxHeight = "850px";
   contentDiv.style.overflowX = "auto";
   contentDiv.style.overflowY = "auto";
 
+  let img3 = document.createElement("img");
+  img3.src = lab3Info.tableTask.task2_23;
+  img3.style.width = "1000px";
+  img3.style.objectFit = "scale-down";
+
   let img = document.createElement("img");
-  img.src = lab3Info.tableTask.rubber_table;
+  img.src = lab3Info.tableTask.task23_div_fixed;
   img.style.width = "1000px";
   img.style.objectFit = "scale-down";
 
-  placeOut.appendChild(contentDiv).appendChild(img);
+  let img2 = document.createElement("img");
+  img2.src = lab3Info.tableTask.task23_div_rubber;
+  img2.style.width = "1000px";
+  img2.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv);
+  contentDiv.appendChild(img3);
+  contentDiv.appendChild(img);
+  contentDiv.appendChild(img2);
 }
 
 export function showFlexScreen() {
@@ -192,16 +225,23 @@ export function showFlexScreen() {
   let contentDiv = document.createElement("div");
   contentDiv.style.paddingTop = "20px";
   contentDiv.style.paddingBottom = "10px";
-  contentDiv.style.height = "800px";
+  contentDiv.style.height = "850px";
   contentDiv.style.overflowX = "auto";
   contentDiv.style.overflowY = "auto";
 
   let img = document.createElement("img");
-  img.src = lab3Info.flexTask.page_screen;
+  img.src = lab3Info.flexTask.flexArScreen;
   img.style.width = "1000px";
   img.style.objectFit = "scale-down";
 
-  placeOut.appendChild(contentDiv).appendChild(img);
+  let img2 = document.createElement("img");
+  img2.src = lab3Info.flexTask.flexVkScreen;
+  img2.style.width = "1000px";
+  img2.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv);
+  contentDiv.appendChild(img);
+  contentDiv.appendChild(img2);
 }
 
 export function showFlexHtml() {
@@ -210,16 +250,23 @@ export function showFlexHtml() {
   let contentDiv = document.createElement("div");
   contentDiv.style.paddingTop = "20px";
   contentDiv.style.paddingBottom = "10px";
-  contentDiv.style.height = "800px";
+  contentDiv.style.height = "850px";
   contentDiv.style.overflowX = "auto";
   contentDiv.style.overflowY = "auto";
 
   let img = document.createElement("img");
-  img.src = lab3Info.flexTask.page_html;
+  img.src = lab3Info.flexTask.flexArHtml;
   img.style.width = "1000px";
   img.style.objectFit = "scale-down";
 
-  placeOut.appendChild(contentDiv).appendChild(img);
+  let img2 = document.createElement("img");
+  img2.src = lab3Info.flexTask.flexVkHtml;
+  img2.style.width = "1000px";
+  img2.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv);
+  contentDiv.appendChild(img);
+  contentDiv.appendChild(img2);
 }
 
 export function showFlexCss() {
@@ -228,16 +275,23 @@ export function showFlexCss() {
   let contentDiv = document.createElement("div");
   contentDiv.style.paddingTop = "20px";
   contentDiv.style.paddingBottom = "10px";
-  contentDiv.style.height = "800px";
+  contentDiv.style.height = "850px";
   contentDiv.style.overflowX = "auto";
   contentDiv.style.overflowY = "auto";
 
   let img = document.createElement("img");
-  img.src = lab3Info.flexTask.page_css;
+  img.src = lab3Info.flexTask.flexArCss;
   img.style.width = "1000px";
   img.style.objectFit = "scale-down";
 
-  placeOut.appendChild(contentDiv).appendChild(img);
+  let img2 = document.createElement("img");
+  img2.src = lab3Info.flexTask.flexVkCss;
+  img2.style.width = "1000px";
+  img2.style.objectFit = "scale-down";
+
+  placeOut.appendChild(contentDiv);
+  contentDiv.appendChild(img);
+  contentDiv.appendChild(img2);
 }
 
 export function showConclusion() {
